2d06c9892c892982733ac0a0e386f9b568e35199
Katherine Ebel <kathyebel@me.com>
2016-04-27 12:05:07 -0400

    updated javascript code

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,9 @@
-console.log("this is a test.");
+$(document).ready(function() {
+  $("form.delete").submit(function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      if (confirm("Are you sure you want to delete this item?")) {
+        this.submit();
+      }
+  });
+});
